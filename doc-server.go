9b7e09b58d997a1c434e7b236be78b2e6d59ff08
Saddam H <thedevsaddam@gmail.com>
2019-04-18 00:26:51 +0600

    Update home.html

--- doc-server.go
+++ doc-server.go
@@ -1,22 +1,35 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
-	"io/ioutil"
+	"html/template"
 	"log"
 	"net/http"
 	"strconv"
 
+	"github.com/fsnotify/fsnotify"
+
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
+// Doc represnet in-memory html/markdown documentation
+type Doc struct {
+	HTML, Markdown string
+}
+
 var (
+	docs         = make(map[string]Doc, 0)
 	dir          string
 	docServerCMD = &cobra.Command{
 		Use:   "doc-server",
 		Short: "Doc run a full documentaion server",
 		Long:  `Doc run a full documentaion server`,
 		Run:   docServer,
+		PreRun: func(cmd *cobra.Command, args []string) {
+			loadConfig()
+		},
 	}
 )
 
@@ -27,6 +40,16 @@ func init() {
 }
 
 func docServer(cmd *cobra.Command, args []string) {
+	// watch configs change
+	go func() {
+		viper.WatchConfig()
+		viper.OnConfigChange(func(e fsnotify.Event) {
+			fmt.Println("Loading new configs...")
+			loadConfig()
+			fmt.Printf("Config [%s] loaded!\n", e.Name)
+		})
+	}()
+
 	http.HandleFunc("/", home)
 	log.Println("Listening on port: ", port)
 	log.Printf("Web Server is available at http://localhost:%s/\n", strconv.Itoa(port))
@@ -43,14 +66,47 @@ func docServer(cmd *cobra.Command, args []string) {
 // 5. User can see individual documentaion (both->html/markdown)
 
 func home(w http.ResponseWriter, r *http.Request) {
-	buf := readJSONtoMarkdownHTML("./example.json")
-	if err := ioutil.WriteFile("./files/example-md.html", buf.Bytes(), 0644); err != nil {
-		panic(err)
-	}
-	bb, err := ioutil.ReadFile("./files/example-md.html")
+	tm := template.New("main")
+	tm.Delims("@{{", "}}@")
+	tm.Funcs(template.FuncMap{
+		"html":            html,
+		"css":             css,
+		"js":              js,
+		"snake":           snake,
+		"color":           color,
+		"trimQueryParams": trimQueryParams,
+		"date_time":       dateTime,
+		"markdown":        markdown,
+	})
+	t, err := tm.Parse(assets.DocServerHome)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
+	}
+	type app struct {
+		Name string
+	}
+	data := struct {
+		Assets Assets
+		Data   app
+	}{
+		Assets: assets,
+		Data: app{
+			Name: viper.GetString("app.name"),
+		},
+	}
+	buf := new(bytes.Buffer)
+	if err := t.Execute(buf, data); err != nil {
+		log.Fatal(err)
 	}
 	w.Header().Add("Content-Type", "text/html")
-	w.Write(bb)
+	w.Write(buf.Bytes())
+}
+
+func loadConfig() {
+	viper.SetConfigName("config") // name of config file (without extension)
+	viper.AddConfigPath(".")      // optionally look for config in the working directory
+	err := viper.ReadInConfig()   // Find and read the config file
+	if err != nil {               // Handle errors reading the config file
+		panic(fmt.Errorf("Fatal error config file: %s", err))
+	}
 }
